a1fd3ad1df41550e230dda130b513de88e79de5a
camilo <cavb.1205@hotmail.com>
2020-04-02 22:20:10 -0300

    se creo el template base.html

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+
+
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return render_template('base.html')
+
+@app.route('/inversion/')
+def inversion_list():
+    inversion_list = [200,400,600,543]
+    context = {'inversion_list':inversion_list}
+    return render_template('inversion/inversion_list.html', **context)
