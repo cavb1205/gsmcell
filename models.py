a4050d50db7326e995f4e95dc36307eba2a8ca48
camilo <cavb.1205@hotmail.com>
2020-04-03 16:05:43 -0300

    se creo el modelo usuario

---
+++ models.py
@@ -0,0 +1,24 @@
+from run import db
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rut = db.Column(db.String(12), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True)
+    first_name = db.Column(db.String(100),nullable=False)
+    last_name = db.Column(db.String(100),nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128))
+    cel = db.Column(db.String(12),nullable=False)
+    cel_opt = db.Column(db.String(12))
+    is_active = db.Column(db.Boolean, default=True)
+    is_admin = db.Column(db.Boolean, default=False)
+    #almacen_id
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+        
--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+
 from flask import Flask, render_template, request
 
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:c@m1lovaron@localhost/gsmcell'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+from models import User
 
 
 
@@ -14,3 +23,10 @@ def inversion_list():
     inversion_list = [200,400,600,543]
     context = {'inversion_list':inversion_list}
     return render_template('inversion/inversion_list.html', **context)
+
+
+@app.route('/usuario/')
+def usuario_list():
+    usuario_list = User.query.all()
+    context = {'usuario_list':usuario_list}
+    return render_template('usuario/usuario_list.html', **context)
